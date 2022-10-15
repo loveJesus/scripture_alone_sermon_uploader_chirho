5e149059ab97020ec684d352f29078f1711ffde5
LoveJesus <loveJesus@loveJes.us>
2022-10-15 00:04:12 -0600

    HALLELUJAH - attempt to fix setup.py for requires

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements_chirho = [
+    "pocketbase==0.1.0",
+    "python-dotenv==0.14.0",
+    "requests==2.24.0",
+    "beautifulsoup4==4.9.1",
+]
+
 setuptools.setup(
     name='scripture_alone_sermon_uploader_chirho',
     version="0.1.0",
@@ -11,6 +18,7 @@ setuptools.setup(
     author="Love Jesus",
     author_email="loveJesus@loveJesus.xyz",
     description="Hallelujah - upload sermons to https://scripturealone.app.",
+    install_requires=requirements_chirho,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/loveJesus/scripture_alone_sermon_uploader_chirho",
